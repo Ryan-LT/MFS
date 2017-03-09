6d0ad73f84339053dafa3c5e216856332d9d14e6
ThinhNu <thinh.le.sdev@gmail.com>
2017-03-09 11:12:46 +0700

    Implement AngularJS and Get some simple $Http Request

--- src/main/java/com/csc/mfs/controller/LoginController.java
+++ src/main/java/com/csc/mfs/controller/LoginController.java
@@ -24,6 +24,11 @@ public class LoginController {
 	@Autowired
 	private UserServiceSecurity userService;
 
+	@RequestMapping(value = "/index") // , method = RequestMethod.GET
+	public String index() {
+		return "index";
+	}
+	
 	@RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
 	public ModelAndView login(){
 		ModelAndView modelAndView = new ModelAndView();
