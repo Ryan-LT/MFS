ba3bf352f74bc5332f7c12117549284a41ba3b0f
GigaSoldier <kytran.ee@gmail.com>
2017-03-14 11:24:30 -0400

    Upload and Download

--- src/main/java/com/csc/mfs/controller/FileUploadController.java
+++ src/main/java/com/csc/mfs/controller/FileUploadController.java
@@ -20,9 +20,12 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Date;
+import java.util.List;
+import java.util.Vector;
 import java.util.stream.Collectors;
 import com.csc.mfs.model.*;
 import com.csc.mfs.repository.FilesRepository;
+import com.csc.mfs.service.DownloadService;
 import com.csc.mfs.service.FileService;
 import com.csc.mfs.service.UserService;
 
@@ -36,11 +39,14 @@ public class FileUploadController {
         this.storageService = storageService;
     }
     @Autowired
+    private FilesRepository fileRepo;
+    @Autowired
     private FileService fileService;
     
     @Autowired
     private StorageProperties storageProp;
-    
+    @Autowired
+    private DownloadService downloadService;
     @Autowired
 	private UserService userService;
 
@@ -54,24 +60,69 @@ public class FileUploadController {
                                 .fromMethodName(FileUploadController.class, "serveFile", path.getFileName().toString())
                                 .build().toString())
                 .collect(Collectors.toList()));
-
+    	
+    	
+//      model.addAttribute("files", storageService
+//      .loadAll()
+//      .map(path -> path.getFileName().toString()) .collect(Collectors.toList()));
+              
+    	
+    	
+//      Path x =Paths.get("s");
+//     
+//      model.addAttribute("files", storageService
+//      .loadAll()
+//      .map(path ->
+//              x)
+//      .collect(Collectors.toList()));
+//    	List<String> x = new Vector();
+//    	x.add("124");
+//    	x.add("12344");
+//    	model.addAttribute("files",x);
+        
         return "mainPage";
     }
     
+    /**
+     * @param This function is for responding to a download request
+     * @return
+     */
     @GetMapping("/files/{filename:.+}")
     public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
-
-        Resource file = storageService.loadAsResource(filename);
-        return ResponseEntity
-                .ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+file.getFilename()+"\"")
-                .body(file);
+    	//get current log-in user
+    	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+ 		User user = userService.findUserByEmail(auth.getName());
+ 		//get download information
+ 		Download downLoad = new Download();
+ 		
+ 		List<Files> listFile = fileRepo.findByName(filename);
+ 		Files fileDownLoad = listFile.get(0);
+ 		
+ 		// check if the user have not reach the maximum download per day.
+ 		if(downloadService.beforeDownload(user.getId(), fileDownLoad.getSize()))
+ 		{
+ 			downLoad.setIdFile(fileDownLoad);
+ 	 		downLoad.setIdUser(user);
+ 	 		downLoad.setDatedownload(new Date());
+ 	 		downloadService.insert(downLoad);
+ 	        Resource file = storageService.loadAsResource(filename);
+ 	        return ResponseEntity
+ 	                .ok()
+ 	                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+file.getFilename()+"\"")
+ 	                .body(file);
+ 		}
+ 		
+ 		Resource file = null;
+ 		return ResponseEntity
+	                .ok()
+	                .body(file);
+        
     }
 
     @PostMapping("/upload")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file,
                                    RedirectAttributes redirectAttributes) {
-
+    	
         storageService.store(file);
         redirectAttributes.addFlashAttribute("message",
                 "You successfully uploaded " + file.getOriginalFilename() + "!");
@@ -79,15 +130,22 @@ public class FileUploadController {
         Path fileDBPath = Paths.get(storageProp.getLocation());
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		User user = userService.findUserByEmail(auth.getName());
-		
-        fileDB.setName(file.getOriginalFilename());
-        fileDB.setSize((double)file.getSize());
-        fileDB.setDateupload(new Date());
-        fileDB.setPath(fileDBPath.resolve(file.getOriginalFilename()).toString());
-        fileDB.setUserId(user.getId());
+		CategoriesType category = new CategoriesType(1);
+		if(fileService.beforeUpload(user.getId(), file.getSize()/1024.0)!=-1){
+			 fileDB.setName(file.getOriginalFilename());
+		        fileDB.setSize((double)file.getSize()/1024.0);
+		        fileDB.setDateupload(new Date());
+		        fileDB.setPath(fileDBPath.resolve(file.getOriginalFilename()).toString());
+		        fileDB.setUserId(user.getId());
+		        fileDB.setIdType(category);
+		        fileService.insertFile(fileDB);
+		        
+		        return ResponseEntity.ok().body("You have successfully uploaded "+file.getOriginalFilename());
+		} else {
+			 return ResponseEntity.ok().body("Fail to upload "+file.getOriginalFilename());
+		}
+       
         
-        fileService.insertFile(fileDB);
-        return "redirect:/upload/";
     }
 
     @ExceptionHandler(StorageFileNotFoundException.class)
