ffe3548999aa0e7c8143138ac400c6bca7ad153f
ThinhNu <thinh.le.sdev@gmail.com>
2017-03-14 09:47:15 +0700

    Relocated all memeber Controllers.js

---
+++ src/main/resources/static/js/controller/memberControllers/personStorageController.js
@@ -0,0 +1,51 @@
+var app = angular.module('myWeb');
+
+app.controller('PersonStorageController', function($scope, $http){
+    //this.files = storage;
+	$scope.count = 0;
+    getFiles('0', '2');
+    countAllFile();
+    $scope.getNumber = function(num) {
+        return new Array(num);
+    }
+    function countAllFile() { 
+		$http({
+			method: 'get',
+			url: "http://localhost:8080/file/countFileOfUser/"+$scope.userId
+		}).success(function(data, status, headers, config){
+			$scope.count = Math.round(data/2);
+		})
+		.error(function(data, status, headers, config){
+			alert("fail");
+		});
+	}
+    
+    function getFiles(page, pageSize) { 
+		$http({
+			method: 'get',
+			url: "http://localhost:8080/file/getByUser/"+$scope.userId+"/"+ parseInt(page)+"/"+ parseInt(pageSize)
+		}).success(function(data, status, headers, config){
+			$scope.files = data;
+		})
+		.error(function(data, status, headers, config){
+			alert("fail");
+		});
+	}
+    
+    $scope.getFileByPage = function(page, pageSize){
+    	getFiles(parseInt(page), parseInt(pageSize));
+    }
+    
+    $scope.deleteFile = function(id){
+    	$http({
+			method: 'get',
+			url: "http://localhost:8080/file/delete/"+id
+		}).success(function(data, status, headers, config){
+			getFiles();
+		})
+		.error(function(data, status, headers, config){
+			alert("fail");
+		});
+    }
+    
+});
