ae1efc16c8b364fee22bb26e84cfa2342169ae5a
GigaSoldier <kytran.ee@gmail.com>
2017-03-14 16:18:22 -0400

    14/3/2017 4h18PM

--- src/main/java/com/csc/mfs/storage/FileSystemStorageService.java
+++ src/main/java/com/csc/mfs/storage/FileSystemStorageService.java
@@ -31,7 +31,7 @@ public class FileSystemStorageService implements StorageService {
             if (file.isEmpty()) {
                 throw new StorageException("Failed to store empty file " + file.getOriginalFilename());
             }
-            Files.copy(file.getInputStream(), this.rootLocation.resolve(file.getOriginalFilename() ));
+            Files.copy(file.getInputStream(), this.rootLocation.resolve(file.getOriginalFilename()));
         } catch (IOException e) {
             throw new StorageException("Failed to store file " + file.getOriginalFilename(), e);
         }
