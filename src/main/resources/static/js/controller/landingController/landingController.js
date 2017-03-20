abc2b1fdc17c20b7b4f8cc63ae310b222cc7c633
ThinhNu <thinh.le.sdev@gmail.com>
2017-03-20 15:12:40 +0700

    Change Show Page number

--- src/main/resources/static/js/controller/landingController/landingController.js
+++ src/main/resources/static/js/controller/landingController/landingController.js
@@ -8,7 +8,7 @@ app.controller("landingController", function($scope, $http, $window, $routeParam
 	  
 	$scope.page = 0;
 	$scope.pageSum;
-	$scope.pageSize = 10;
+	$scope.pageSize = 8;
 	$scope.selectedIndex = 0;
 	$scope.infoSearch = undefined;
 	
